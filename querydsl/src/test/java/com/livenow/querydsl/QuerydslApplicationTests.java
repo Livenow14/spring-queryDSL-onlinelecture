c7d62ed0138e598917954b9182679b540d6e61fb
Livenow14 <xowjd41@naver.com>
2020-07-31 17:04:11 +0900

    프로젝트 환경설정_프로젝트 생성
     tset 동작확인
    helloController  동작확인

---
+++ querydsl/src/test/java/com/livenow/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livenow.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
