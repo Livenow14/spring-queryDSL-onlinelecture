b2bb18d965a80f9a8cd2cb49211c5025645c7c97
Livenow14 <xowjd41@naver.com>
2020-08-06 14:49:02 +0900

    기본 문법_집합

--- querydsl/src/test/java/com/livenow/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/com/livenow/querydsl/QuerydslBasicTest.java
@@ -3,8 +3,10 @@ package com.livenow.querydsl;
 
 import com.livenow.querydsl.domain.Member;
 import com.livenow.querydsl.domain.QMember;
+import com.livenow.querydsl.domain.QTeam;
 import com.livenow.querydsl.domain.Team;
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -18,6 +20,7 @@ import javax.persistence.EntityManager;
 import java.util.List;
 
 import static com.livenow.querydsl.domain.QMember.member;
+import static com.livenow.querydsl.domain.QTeam.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -38,9 +41,9 @@ public class QuerydslBasicTest {
         Team teamB = Team.builder().name("teamB").build();
         
         Member member1 = Member.builder().username("member1").age(10).team(teamA).build();
-        Member member2 = Member.builder().username("member2").age(10).team(teamA).build();
-        Member member3 = Member.builder().username("member3").age(10).team(teamB).build();
-        Member member4 = Member.builder().username("member4").age(10).team(teamB).build();
+        Member member2 = Member.builder().username("member2").age(20).team(teamA).build();
+        Member member3 = Member.builder().username("member3").age(30).team(teamB).build();
+        Member member4 = Member.builder().username("member4").age(40).team(teamB).build();
 
 /*        em.persist(member1);
         em.persist(member2);
@@ -294,6 +297,61 @@ public class QuerydslBasicTest {
         assertThat(queryResults.getOffset()).isEqualTo(1);
 
         assertThat(queryResults.getResults().size()).isEqualTo(2);
+    }
+
+    @DisplayName("집합")
+    @Test
+    public void aggregation() throws Exception{
+        //given
+        /**
+         * queryDsl이 제공하는 tuple
+         * 튜플은 많이 쓰진않고 dto를 통해서 해결함
+         */
+        List<Tuple> result = queryFactory
+                .select(
+                        member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min()
+                )
+                .from(member)
+                .fetch();
+
+        //when
+        Tuple tuple = result.get(0);
+        //then
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(member.age.max())).isEqualTo(40);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
+    }
+
+    /**
+     * 팀의 이름과 각 팀의 평균 연령을 구해라
+     *
+     */
+    @DisplayName("Group")
+    @Test
+    public void group() throws Exception{
+        //given
+        List<Tuple> result = queryFactory
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name)
+                .fetch();
+        //when
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
+
+        //then
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
     }
 
 }
