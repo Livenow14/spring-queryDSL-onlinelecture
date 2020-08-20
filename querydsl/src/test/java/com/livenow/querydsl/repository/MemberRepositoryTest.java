f115e2be01eb8233ab8cb205450882e3109f6b04
Livenow14 <xowjd41@naver.com>
2020-08-20 15:33:20 +0900

    실무 활용 - 스프링 데이터 JPA와 Querydsl_사용자 정의 리포지토리

---
+++ querydsl/src/test/java/com/livenow/querydsl/repository/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.livenow.querydsl.repository;
+
+import com.livenow.querydsl.domain.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void basicTest() {
+        Member member = new Member("member1", 10);
+        memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        assertThat(member).isEqualTo(findMember);
+
+        List<Member> result = memberRepository.findAll();
+        assertThat(result).containsExactly(member);
+
+        List<Member> result2 = memberRepository.findByUsername("member1");
+        assertThat(result2).containsExactly(member);
+
+    }
+}
