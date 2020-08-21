70d411d8802cefb4a1e092d1df4daaf30c28f37d
Livenow14 <xowjd41@naver.com>
2020-08-21 16:29:54 +0900

    실무 활용 - 스프링 데이터 JPA와 Querydsl_스프링 데이터 페이징 활용3-컨트롤러 개발

--- querydsl/src/main/java/com/livenow/querydsl/controller/MemberController.java
+++ querydsl/src/main/java/com/livenow/querydsl/controller/MemberController.java
@@ -2,9 +2,14 @@ package com.livenow.querydsl.controller;
 
 import com.livenow.querydsl.dto.MemberTeamDto;
 import com.livenow.querydsl.repository.MemberJpaRepository;
+import com.livenow.querydsl.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
@@ -13,13 +18,28 @@ import java.util.List;
 public class MemberController {
 
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * http://localhost:8080/v1/member?teamName=teamB&ageGoe=31&ageLoe=35
      * 이런식으로 조회 가능하다
      */
-    @GetMapping("/v1/member")
+    @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.search(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable){
+        return memberRepository.searchPageSimple(condition, pageable);
+    }
+
+    /**
+     * http://localhost:8080/v3/members?page=0&size=110 이렇게하면 countquery를 안날림
+     * @return
+     */
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable){
+        return memberRepository.searchPageComplex(condition, pageable);
     }
 }
